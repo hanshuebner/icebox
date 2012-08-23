5e2b42cf0ddf546142a0bc287af69f974ef4ae87
Hans Huebner <hans.huebner@gmail.com>
2012-08-24 00:11:59 +0200

    add tests

---
+++ test/buster.js
@@ -0,0 +1,9 @@
+var config = module.exports;
+
+config["icebox tests"] = {
+    rootPath: "../",
+    environment: "node",
+    tests: [
+        "test/*-test.js"
+    ]
+}
---
+++ test/date-test.js
@@ -0,0 +1,16 @@
+var icebox = require('../lib/icebox');
+var buster = require("buster");
+
+buster.testCase("Date support", {
+    "Date is preserved through freeze/thaw": function() {
+        var timestamp = new Date();
+        var original = { timestamp: timestamp };
+        var frozen = icebox.freeze(original);
+        var thawed = icebox.thaw(frozen);
+        assert.equals(timestamp.getTime(), thawed.timestamp.getTime(), 'Date object could not be restored');
+    },
+    "same Date serialized two times yields same frozen represenation": function() {
+        var data = { now: new Date() };
+        assert.equals(icebox.freeze(data), icebox.freeze(data), 'Same object containing Date yielded different external representations');
+    }
+});
